44336c1e1d69eae1c3cde706b8cfe70ac197c176
Daniel Higgins <daniel.higgins7@btinternet.com>
2023-12-19 11:47:34 +0000

    Added the cascading delete functionality for companies and their employees

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -7,6 +7,8 @@
         {{-- <a :href="route('/companies/create')">Add new</a> --}}
         <x-responsive-nav-link :href="route('companies.create')">Add new</x-responsive-nav-link>
 
+        <h2>* Deleting a company will delete all employees assosciated with that company</h2>
+
         <div class="flex flex-col">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
